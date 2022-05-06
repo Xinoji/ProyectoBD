ade7a3e59d63c9a86a68dd1141f457fc9178de60
AlanOcampo <98377389+AlanOcampo@users.noreply.github.com>
2022-05-05 21:36:20 -0500

    inventario mostrar-modificacion

--- Logica/refacciones.cs
+++ Logica/refacciones.cs
@@ -11,15 +11,6 @@ using ProyectoBD.BD;
 
 namespace ProyectoBD.Logica
 {
-    public struct refaccionesData
-    {
-        string codigo;
-        int stock;
-        decimal precio;
-        decimal precioT;
-        string descripcion;
-    }
-
     class refacciones : DbConnection
     {
         
@@ -32,50 +23,10 @@ namespace ProyectoBD.Logica
                 da.SelectCommand = command;
                 DataTable dt = new DataTable();
                 da.Fill(dt);
-                return dt;
+                return dt; 
             }
 
         }
-
-        /*
-        public void Upload(/*string Codigo/*, int Stock, decimal Precio, decimal PrecioT,string Descripcion)
-        {
-            using(var conexion= GetConnection())
-            {
-                conexion.Open();
-                using(var command = new SqlCommand())
-                {
-                    command.Connection = conexion;
-                    command.CommandText = "update descripcion set=@descripcion where codigo = @codigo ";
-                    command.Parameters.Add("@descripcion", System.Data.SqlDbType.VarChar).Value =Descripcion;
-                    command.ExecuteNonQuery();
-
-                }
-            }
-        }*/
-
-        /*
-        public void Upload(string Codigo, int Stock, decimal Precio, decimal PrecioT,string Descripcion)
-        {
-            using (var connection = GetConnection())
-            {
-                connection.Open();
-                using (var command = new SqlCommand())
-                {
-
-                    command.Connection = connection;
-                    command.CommandText = "update refaccion set codigo = @codigo,Stock=@Stock, precio = @precio, precio_T = @precio_T, Descripcion = @Descripcion where codigo = @codigo";
-                    command.Parameters.Add("@codigo", System.Data.SqlDbType.VarChar).Value = Codigo;
-                    command.Parameters.Add("@Stock", System.Data.SqlDbType.Int).Value = Stock;
-                    command.Parameters.Add("@precio", System.Data.SqlDbType.Decimal).Value = Precio;
-                    command.Parameters.Add("@precio_T", System.Data.SqlDbType.Decimal).Value = PrecioT;
-                    command.Parameters.Add("@Descripcion", System.Data.SqlDbType.VarChar).Value = Descripcion;
-                    command.ExecuteNonQuery();
-                }
-            }
-
-        }*/
-
         
 
         public class DataGridViewUtils
