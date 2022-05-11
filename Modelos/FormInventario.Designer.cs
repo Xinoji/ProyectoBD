462448827fbe39d6bf8c638db99b652c22ec8e39
AlanOcampo <98377389+AlanOcampo@users.noreply.github.com>
2022-05-10 19:43:07 -0500

    Add files via upload

--- Modelos/FormInventario.Designer.cs
+++ Modelos/FormInventario.Designer.cs
@@ -28,11 +28,17 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle7 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle8 = new System.Windows.Forms.DataGridViewCellStyle();
             this.dgvinventario = new System.Windows.Forms.DataGridView();
             this.btnaActualizar = new System.Windows.Forms.Button();
-            this.Buscar = new System.Windows.Forms.Button();
+            this.btnBuscar = new System.Windows.Forms.Button();
             this.txtCodigo = new System.Windows.Forms.TextBox();
             this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.btnSumar = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtCantidad = new System.Windows.Forms.TextBox();
             ((System.ComponentModel.ISupportInitialize)(this.dgvinventario)).BeginInit();
             this.SuspendLayout();
             // 
@@ -41,59 +47,133 @@
             this.dgvinventario.AllowUserToAddRows = false;
             this.dgvinventario.AllowUserToDeleteRows = false;
             this.dgvinventario.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            dataGridViewCellStyle7.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
+            dataGridViewCellStyle7.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            dataGridViewCellStyle7.ForeColor = System.Drawing.SystemColors.WindowText;
+            dataGridViewCellStyle7.NullValue = null;
+            dataGridViewCellStyle7.SelectionBackColor = System.Drawing.SystemColors.Highlight;
+            dataGridViewCellStyle7.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
+            dataGridViewCellStyle7.WrapMode = System.Windows.Forms.DataGridViewTriState.True;
+            this.dgvinventario.ColumnHeadersDefaultCellStyle = dataGridViewCellStyle7;
             this.dgvinventario.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dataGridViewCellStyle8.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
+            dataGridViewCellStyle8.BackColor = System.Drawing.SystemColors.Window;
+            dataGridViewCellStyle8.Font = new System.Drawing.Font("Times New Roman", 8.25F);
+            dataGridViewCellStyle8.ForeColor = System.Drawing.SystemColors.ControlText;
+            dataGridViewCellStyle8.SelectionBackColor = System.Drawing.SystemColors.Highlight;
+            dataGridViewCellStyle8.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
+            dataGridViewCellStyle8.WrapMode = System.Windows.Forms.DataGridViewTriState.False;
+            this.dgvinventario.DefaultCellStyle = dataGridViewCellStyle8;
+            this.dgvinventario.EnableHeadersVisualStyles = false;
             this.dgvinventario.Location = new System.Drawing.Point(12, 12);
             this.dgvinventario.Name = "dgvinventario";
-            this.dgvinventario.Size = new System.Drawing.Size(556, 426);
-            this.dgvinventario.TabIndex = 0;
+            this.dgvinventario.RowHeadersVisible = false;
+            this.dgvinventario.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
+            this.dgvinventario.Size = new System.Drawing.Size(907, 657);
+            this.dgvinventario.TabIndex = 5;
             this.dgvinventario.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dgvinventario_CellContentClick);
             // 
             // btnaActualizar
             // 
-            this.btnaActualizar.Location = new System.Drawing.Point(622, 207);
+            this.btnaActualizar.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F);
+            this.btnaActualizar.ForeColor = System.Drawing.Color.White;
+            this.btnaActualizar.Location = new System.Drawing.Point(929, 569);
             this.btnaActualizar.Name = "btnaActualizar";
-            this.btnaActualizar.Size = new System.Drawing.Size(134, 71);
-            this.btnaActualizar.TabIndex = 1;
+            this.btnaActualizar.Size = new System.Drawing.Size(323, 100);
+            this.btnaActualizar.TabIndex = 2;
             this.btnaActualizar.Text = "Actualizar Datos";
             this.btnaActualizar.UseVisualStyleBackColor = true;
             this.btnaActualizar.Click += new System.EventHandler(this.btnaActualizar_Click);
             // 
-            // Buscar
+            // btnBuscar
             // 
-            this.Buscar.Location = new System.Drawing.Point(688, 105);
-            this.Buscar.Name = "Buscar";
-            this.Buscar.Size = new System.Drawing.Size(100, 20);
-            this.Buscar.TabIndex = 2;
-            this.Buscar.Text = "Buscar Producto";
-            this.Buscar.UseVisualStyleBackColor = true;
-            this.Buscar.Click += new System.EventHandler(this.Buscar_Click);
+            this.btnBuscar.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F);
+            this.btnBuscar.ForeColor = System.Drawing.Color.White;
+            this.btnBuscar.Location = new System.Drawing.Point(1123, 228);
+            this.btnBuscar.Name = "btnBuscar";
+            this.btnBuscar.Size = new System.Drawing.Size(129, 35);
+            this.btnBuscar.TabIndex = 1;
+            this.btnBuscar.Text = "Buscar Producto";
+            this.btnBuscar.UseVisualStyleBackColor = true;
+            this.btnBuscar.Click += new System.EventHandler(this.Buscar_Click);
             // 
             // txtCodigo
             // 
-            this.txtCodigo.Location = new System.Drawing.Point(688, 64);
+            this.txtCodigo.Font = new System.Drawing.Font("Microsoft Sans Serif", 13F);
+            this.txtCodigo.ForeColor = System.Drawing.Color.White;
+            this.txtCodigo.Location = new System.Drawing.Point(1123, 179);
             this.txtCodigo.Name = "txtCodigo";
-            this.txtCodigo.Size = new System.Drawing.Size(100, 20);
-            this.txtCodigo.TabIndex = 4;
+            this.txtCodigo.Size = new System.Drawing.Size(129, 27);
+            this.txtCodigo.TabIndex = 0;
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(574, 67);
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 13F);
+            this.label1.ForeColor = System.Drawing.SystemColors.Window;
+            this.label1.Location = new System.Drawing.Point(925, 182);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(108, 13);
+            this.label1.Size = new System.Drawing.Size(181, 22);
             this.label1.TabIndex = 6;
             this.label1.Text = "Codigo Del Producto:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 30F);
+            this.label2.ForeColor = System.Drawing.SystemColors.Window;
+            this.label2.Location = new System.Drawing.Point(993, 26);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(193, 46);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "Inventario";
+            // 
+            // btnSumar
+            // 
+            this.btnSumar.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F);
+            this.btnSumar.ForeColor = System.Drawing.Color.White;
+            this.btnSumar.Location = new System.Drawing.Point(1123, 394);
+            this.btnSumar.Name = "btnSumar";
+            this.btnSumar.Size = new System.Drawing.Size(129, 35);
+            this.btnSumar.TabIndex = 8;
+            this.btnSumar.Text = "Calcular";
+            this.btnSumar.UseVisualStyleBackColor = true;
+            this.btnSumar.Click += new System.EventHandler(this.btnSumar_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F);
+            this.label3.ForeColor = System.Drawing.SystemColors.Window;
+            this.label3.Location = new System.Drawing.Point(925, 347);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(191, 18);
+            this.label3.TabIndex = 10;
+            this.label3.Text = "Cantidad ingresada/retirada:";
+            // 
+            // txtCantidad
+            // 
+            this.txtCantidad.Font = new System.Drawing.Font("Microsoft Sans Serif", 13F);
+            this.txtCantidad.ForeColor = System.Drawing.Color.White;
+            this.txtCantidad.Location = new System.Drawing.Point(1123, 343);
+            this.txtCantidad.Name = "txtCantidad";
+            this.txtCantidad.Size = new System.Drawing.Size(129, 27);
+            this.txtCantidad.TabIndex = 11;
             // 
             // FormInventario
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(1264, 681);
+            this.Controls.Add(this.txtCantidad);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.btnSumar);
+            this.Controls.Add(this.dgvinventario);
+            this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.txtCodigo);
-            this.Controls.Add(this.Buscar);
+            this.Controls.Add(this.btnBuscar);
             this.Controls.Add(this.btnaActualizar);
-            this.Controls.Add(this.dgvinventario);
             this.Name = "FormInventario";
             this.Text = "FormInventario";
             this.Load += new System.EventHandler(this.FormInventario_Load);
@@ -107,8 +187,12 @@
 
         private System.Windows.Forms.DataGridView dgvinventario;
         private System.Windows.Forms.Button btnaActualizar;
-        private System.Windows.Forms.Button Buscar;
+        private System.Windows.Forms.Button btnBuscar;
         private System.Windows.Forms.TextBox txtCodigo;
         private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button btnSumar;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtCantidad;
     }
 }
--- Modelos/FormInventario.cs
+++ Modelos/FormInventario.cs
@@ -2,6 +2,7 @@
 using ProyectoBD.Logica;
 using System;
 using System.Data;
+using System.Drawing;
 using System.Data.SqlClient;
 using System.Windows.Forms;
 using static ProyectoBD.Logica.refacciones;
@@ -13,10 +14,12 @@ namespace ProyectoBD.Modelos
         SqlConnection conexion = new SqlConnection("Server =localhost\\SQLEXPRESS; Database = Lito; Trusted_Connection = True; ");
         refacciones refac = new refacciones();
 
+       
+
         public FormInventario()
         {
             InitializeComponent();
-
+            setTheme();
         }
         public void ReloadTable(){
             dgvinventario.DataSource = refac.MostrarRefacciones();
@@ -37,25 +40,42 @@ namespace ProyectoBD.Modelos
 
         public void UpdateD()
         {
+            
             for (int i = 0; i <= dgvinventario.Rows.Count - 1; i++)
             {
-                SqlCommand command = new SqlCommand("Update Refaccion set Stock=@Stock,precio=@precio,Descripcion=@Descripcion where codigo=@codigo; ", conexion);
+                
+                SqlCommand command = new SqlCommand("Update Refaccion set Stock=@Stock,precio=@precio,Descripcion=@Descripcion,cant_min=@cant_min, cant_max=@cant_max  where codigo=@codigo; ", conexion);
                 command.Parameters.AddWithValue("@Stock", dgvinventario.Rows[i].Cells[1].Value ?? (object)DBNull.Value);
                 command.Parameters.AddWithValue("@precio", dgvinventario.Rows[i].Cells[2].Value ?? (object)DBNull.Value);
                 //command.Parameters.AddWithValue("@precioT", dgvinventario.Rows[i].Cells[3].Value);
                 command.Parameters.AddWithValue("@Descripcion", dgvinventario.Rows[i].Cells[4].Value ?? (object)DBNull.Value);
-                command.Parameters.AddWithValue("codigo", dgvinventario.Rows[i].Cells[0].Value ?? (object)DBNull.Value);
+                command.Parameters.AddWithValue("@cant_min", dgvinventario.Rows[i].Cells[5].Value ?? (object)DBNull.Value);
+                command.Parameters.AddWithValue("@cant_max", dgvinventario.Rows[i].Cells[6].Value ?? (object)DBNull.Value);
+                command.Parameters.AddWithValue("codigo", dgvinventario.Rows[i].Cells[0].Value ?? (object)DBNull.Value);               
                 conexion.Open();
                 command.ExecuteNonQuery();
                 conexion.Close();
             }
             MessageBox.Show("Actualizado");
         }
+        public void UpdateSuma()
+        {
+
+
+                SqlCommand command = new SqlCommand("UPDATE Refaccion SET Stock = Stock + @Parm1  where codigo=@codigo; ", conexion);
+                command.Parameters.AddWithValue("@Parm1", int.Parse(txtCantidad.Text));
+                command.Parameters.AddWithValue("@codigo", txtCodigo.Text);
+                conexion.Open();
+                command.ExecuteNonQuery();
+                conexion.Close();
+            
+            MessageBox.Show("Actualizado");
+        }
+
 
         private void btnaActualizar_Click(object sender, EventArgs e)
         {
             UpdateD();
-
             ReloadTable();
         }
 
@@ -77,6 +97,48 @@ namespace ProyectoBD.Modelos
         }
 
 
+
+        private void btnSumar_Click(object sender, EventArgs e)
+        {
+            UpdateSuma();
+            ReloadTable();
+        }
+
+        private void setTheme()
+        {
+            var themeColor = WinTheme.GetAccentColor();//Windows Accent Color
+            var lightColor = ControlPaint.Light(themeColor);
+            var darkColor = ControlPaint.Dark(themeColor);
+            var lightlight = ControlPaint.LightLight(themeColor);
+            var lightdarkdarkColor = ControlPaint.Light(ControlPaint.Dark(darkColor));
+            var darklightlight = ControlPaint.Dark(lightlight);
+            var lightlightInvert = ControlPaint.LightLight(ControlPaint.Dark(WinTheme.InvertMeAColour(themeColor)));
+
+            BackColor = darkColor;
+
+
+            btnaActualizar.BackColor = themeColor;
+            btnaActualizar.FlatAppearance.BorderColor = lightColor;
+            btnBuscar.BackColor = themeColor;
+            btnBuscar.FlatAppearance.BorderColor = lightColor;
+            btnSumar.BackColor = themeColor;
+            btnSumar.FlatAppearance.BorderColor = lightColor;
+            txtCodigo.BackColor = themeColor;
+            txtCantidad.BackColor = themeColor;
+
+
+            dgvinventario.BackgroundColor = lightdarkdarkColor;
+            dgvinventario.DefaultCellStyle.BackColor = lightdarkdarkColor;
+            dgvinventario.DefaultCellStyle.ForeColor = Color.White;
+            dgvinventario.DefaultCellStyle.SelectionForeColor = Color.White;
+            dgvinventario.DefaultCellStyle.SelectionBackColor = lightlightInvert;
+            dgvinventario.ColumnHeadersDefaultCellStyle.BackColor = lightdarkdarkColor;
+            dgvinventario.ColumnHeadersDefaultCellStyle.ForeColor = Color.White;
+            dgvinventario.ColumnHeadersDefaultCellStyle.SelectionForeColor = Color.White;
+            dgvinventario.ColumnHeadersDefaultCellStyle.SelectionBackColor = lightdarkdarkColor;
+
+            dgvinventario.GridColor = darkColor;
+        }
     }
 
 
